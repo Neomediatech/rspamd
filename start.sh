7c973d9cd685c86cfa0d1e3c394ca62b1401c81c
Dario B <feddar@users.noreply.github.com>
2018-11-11 16:51:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ PASSWORD=${PASSWORD:-"SomeSecurePassword"}
 ENABLE_PASSWORD=${ENABLE_PASSWORD:-$PASSWORD}
 
 cat << EOF > /etc/rspamd/local.d/worker-controller.inc
+bind_socket = "0.0.0.0:11334";
 secure_ip = "${SECURE_IP}";
 password = "${PASSWORD}";
 enable_password = "${PASSWORD}";
